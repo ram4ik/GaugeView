f22b5d2a24b2be819513b0873c0fc3b14ce2e2f5
Ramill <ramill_ibragimov@icloud.com>
2022-08-02 13:21:57 +0300

    Initial Commit

---
+++ GaugeView/GaugeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugeViewApp.swift
+//  GaugeView
+//
+//  Created by Ramill Ibragimov on 02.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct GaugeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
